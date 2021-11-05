e47b096086beb2af8fd9f94701bd2f673536707e
wagnerberna <cabelo29007@gmail.com>
2021-11-05 07:50:48 -0300

    back variáveis de ambiente

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql2/promise");
+require('dotenv').config();
 
 const conn = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '123',
+  host: process.env.HOSTNAME,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: 'teste_webmotors',
 });
 
